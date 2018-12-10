88a8c78f0c81d375e5e02de31a615e4e6a3f185b
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-12-10 15:12:07 +0100

    Updated github pages url.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// navigator.serviceWorker.getRegistrations().then(function(registrations) {
+//   for(let registration of registrations) {
+//    registration.unregister()
+//   }
+// })
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
