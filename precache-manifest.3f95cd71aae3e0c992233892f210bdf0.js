8b1a3f3341c463d0920d167aca0400dc519ddc57
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-12-12 11:52:34 +0100

    Updates

---
+++ precache-manifest.3f95cd71aae3e0c992233892f210bdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f2b33ae89f678679e24",
+    "url": "/friendship/static/css/main.00f2cd0e.chunk.css"
+  },
+  {
+    "revision": "0f2b33ae89f678679e24",
+    "url": "/friendship/static/js/main.0f2b33ae.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/friendship/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "523e85086c97807ef21f",
+    "url": "/friendship/static/js/runtime~main.523e8508.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/friendship/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b0a9c86d558919f059cc43501682c0be",
+    "url": "/friendship/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7cd93213fdea38c2fcfbe0f7d07ab0e.js"
+  "/friendship/precache-manifest.3f95cd71aae3e0c992233892f210bdf0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/friendship/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
