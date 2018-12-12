435ab6ea63a470342994cb5b321d22c38b12f4c4
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-12-12 11:45:15 +0100

    Updates

---
+++ precache-manifest.a7cd93213fdea38c2fcfbe0f7d07ab0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f70782bc6230340547c",
+    "url": "/static/css/main.00f2cd0e.chunk.css"
+  },
+  {
+    "revision": "8f70782bc6230340547c",
+    "url": "/static/js/main.8f70782b.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9dfa5ef89792ccb65720f3afb694299",
+    "url": "/index.html"
+  }
+];
